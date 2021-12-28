47bbbe44a35d46b1fe62b99f6cab26f6119ced2b
yuanyazhen001 <yuanyazhen001@ke.com>
2021-12-28 18:08:13 +0800

    refactor: update base infra

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,18 @@
 console.log('This is the background page.');
-console.log('Put the background scripts here.');
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Put the background scripts here.');
+
+  chrome.contextMenus.create({
+    id: 'sampleContextMenu',
+    title: 'Sample Context Menu',
+    contexts: ['selection'],
+  });
+});
+
+let color = '#3aa757';
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,6 @@ if (fileSystem.existsSync(secretsPath)) {
 var options = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
     background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
@@ -130,6 +129,15 @@ var options = {
         },
       ],
     }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: 'src/pages/Background/background.js',
+    //       to: path.join(__dirname, 'build'),
+    //       force: true,
+    //     },
+    //   ],
+    // }),
     new CopyWebpackPlugin({
       patterns: [
         {
@@ -156,12 +164,6 @@ var options = {
           force: true,
         },
       ],
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.html'),
-      filename: 'newtab.html',
-      chunks: ['newtab'],
-      cache: false,
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'pages', 'Options', 'index.html'),
