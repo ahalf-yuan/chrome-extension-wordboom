09d0435eab55750725807fbf17911e8f5854562c
yuanyazhen <yazhener@icloud.com>
2022-01-01 10:13:22 +0800

    refactor: change style

--- src/pages/Content/views/MiniPanel/service.js
+++ src/pages/Content/views/MiniPanel/service.js
@@ -7,32 +7,35 @@
 const YOUDAO_API = 'https://aidemo.youdao.com/trans';
 
 function transDataToFormData(obj) {
-    const res = new URLSearchParams();
-    Object.entries(obj).forEach(([key, value]) => {
-        res.append(key, value);
-    });
-    return res;
+  const res = new URLSearchParams();
+  Object.entries(obj).forEach(([key, value]) => {
+    res.append(key, value);
+  });
+  return res;
 }
 
 function postTranslate(selectedText) {
-    return fetch(YOUDAO_API, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: transDataToFormData({
-            q: selectedText
-        })
-    }).then(res => res.json())
-    .then(res => {
-        return res;
-    }).catch((err) => {
-        // handle error
+  return fetch(YOUDAO_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: transDataToFormData({
+      q: selectedText,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      return res;
     })
+    .catch((err) => {
+      // handle error
+      return null;
+    });
 }
 
 function createWordItem(data) {
-    // return
+  // return
 }
 
-export { postTranslate, createWordItem }
+export { postTranslate, createWordItem };
